e8a7f2ca5b6a364cf863258400ccb794bacbbe62
Manohar Lakkoju <manoharvirat@gmail.com>
2023-06-24 16:57:09 +0530

    feat: backend multer file upload

--- src/app/api/upload/upload.ts
+++ src/app/api/upload/upload.ts
@@ -1,35 +1,43 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
-// import multer from 'multer';
-// import { Buffer } from 'buffer';
+import { NextApiRequest, NextApiResponse } from 'next';
+import multer, { Options, Multer } from 'multer';
+import { Buffer } from 'buffer';
 
-// const upload = multer({
-//   storage: multer.memoryStorage(),
-//   limits: { fileSize: 1024 * 1024 }, // 1MB limit
-//   fileFilter: function (req, file, cb) {
-//     if (!['text/plain', 'text/csv', 'application/json'].includes(file.mimetype)) {
-//       return cb(new Error('Invalid file type'), false);
-//     }
-//     cb(null, true);
-//   },
-// });
+interface MulterRequest extends NextApiRequest {
+  file: any;
+}
 
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
+const upload: Multer = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 1024 * 1024 }, // 1MB limit
+  fileFilter: (req, file, cb) => {
+    if (!['text/plain', 'text/csv', 'application/json'].includes(file.mimetype)) {
+        // @ts-ignore
+      cb(new Error('Invalid file type'), false);
+    } else {
+      cb(null, true);
+    }
+  },
+} as Options);
 
-// export default async function uploadFile(req: NextApiRequest, res: NextApiResponse) {
-//   upload.single('file')(req, res, function (err: any) {
-//     if (err) {
-//       res.status(400).send({ error: err.message });
-//       return;
-//     }
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
-//     // Access the uploaded file from req.file.
-//     const fileContent = (req.file.buffer as Buffer).toString();
-//     console.log(fileContent);
+export default function uploadFile(req: MulterRequest, res: NextApiResponse) {
+    // @ts-ignore
+  upload.single('file')(req, res, (err: Error | null) => {
+    if (err) {
+      res.status(400).send({ error: err.message });
+    } else if(req.file){
+      // Access the uploaded file from req.file.
+      const fileContent = (req.file.buffer as Buffer).toString('utf-8');
+      console.log(fileContent);
 
-//     res.send('File uploaded successfully');
-//   });
-// }
+      res.send('File uploaded successfully');
+    } else {
+      res.status(400).send({ error: "No file uploaded" });
+    }
+  });
+}
