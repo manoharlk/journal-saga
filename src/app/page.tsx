7b9576c00440ab4dd6f5ba1b57a1416ffe24e874
Manohar Lakkoju <manoharvirat@gmail.com>
2023-06-25 01:32:53 +0530

    fix: prerender error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,23 @@ import useLocalStorage from "./lib/hooks/use-local-storage";
 import Onboarding from "../ui/onboarding";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
-  let userData = null;
-  if (typeof window !== undefined) {
-    userData = window?.localStorage?.getItem("userData");
-    if (!userData) {
-      redirect("/onboarding");
-    } else {
-      redirect('/dashboard');
+  useEffect(() => {
+    let userData = null;
+    if (typeof window !== undefined) {
+      userData = window?.localStorage?.getItem("userData");
+      if (!userData) {
+        redirect("/onboarding");
+      } else {
+        redirect('/dashboard');
+      }
     }
-  }
+  }, []);
   return (
     <>
-      
+
     </>
   );
 }
